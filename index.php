215c3b2e96213e10e2b4f1ce810eca7ac1bfdc06
dzAhmed <ouabel@live.com>
2017-12-28 10:28:36 +0100

    Set a router

--- index.php
+++ index.php
@@ -4,4 +4,25 @@ $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
 $db->setAttribute(PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
 
-var_dump($db);
+if (isset($_GET['action']))
+{
+	$action = $_GET['action'];
+	switch ($action)
+	{
+		case 'post':
+			if (isset($_GET['id']) && (int) $_GET['id'] > 0)
+			{
+				var_dump(intval($_GET['id']));
+			}
+			else
+			{
+				echo 'Aucun identifiant d\'article envoyé';
+			}
+		break;
+
+	}
+}
+else
+{
+	echo 'Home';
+}
