39bc2dc78e0bb29008ffd825b4e25cdd137e0826
dzAhmed <ouabel@live.com>
2018-01-19 19:26:30 +0100

    Separating frontend and backend style files

--- view/backend/template.php
+++ view/backend/template.php
@@ -6,7 +6,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link href="public/css/style.css" rel="stylesheet" />
+  <link href="admin/css/style.css" rel="stylesheet" />
   <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, author-scalable=0'>
 </head>
 <body>
